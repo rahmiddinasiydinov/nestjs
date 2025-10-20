e58e52f2a16458669b964a7c85ef68bf86ad0796
Rakhmiddin.Asiydinov <rahmiddinasiydinov@example.com>
2025-10-20 15:41:34 +0500

    Section 14

--- car-value/test/auth.e2e-spec.ts
+++ car-value/test/auth.e2e-spec.ts
@@ -14,12 +14,10 @@ describe('Authentication System (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-    
-    
   });
 
   it('handles a signup request', () => {
-    const email = 'ray1234@gmail.com';
+    const email = 'ray12345@gmail.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email: email, password: '12345678' })
@@ -30,4 +28,23 @@ describe('Authentication System (e2e)', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'abc@abc.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '12345678' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie || [''])
+      .expect(200);
+      
+      expect(body.email).toEqual(email);
+  });
+
 });
