39267ba4d788da2bc73c74bf568124f62c8d89f4
Rakhmiddin.Asiydinov <rahmiddinasiydinov@example.com>
2025-10-29 13:39:20 +0500

    Relations with TypeORM

--- car-value/src/reports/reports.service.ts
+++ car-value/src/reports/reports.service.ts
@@ -3,14 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportData: CreateReportDto) {
+  create(reportData: CreateReportDto, user: User) {
     const report = this.repo.create(reportData);
-
-    return this.repo.save(report  );
+    report.user = user;
+    return this.repo.save(report);
   }
 }
